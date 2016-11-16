9dfb6308ccc4eba6f6639c0e00facc7be38298cc
Joshua Maximillian <m26415012@opensource.petra.ac.id>
2016-11-16 20:12:11 +0700

    wow2

---
+++ pushmingguan2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print 'Function python\n'
+def sayHello():
+ print '(?)'
+def kali(bil1, bil2):
+ print int(bil1)*int(bil2)
+bil1=raw_input('Angka 1 = ')
+bil2=raw_input('Angka 2 = ')
+sayHello()
+print 'Coba fungsi kali = '
+kali(bil1, bil2)
